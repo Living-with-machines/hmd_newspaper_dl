d1dae15614f289461ba7e88106b6523fba3af557
Daniel van Strien <davanstrien@gmail.com>
2022-01-06 16:06:12 +0000

    update url for hmd newspapers collection

--- hmd_newspaper_dl/core.py
+++ hmd_newspaper_dl/core.py
@@ -14,7 +14,8 @@ import time
 from collections import namedtuple
 from functools import lru_cache
 from operator import itemgetter
-#from os import umask
+
+# from os import umask
 import os
 from pathlib import Path
 from typing import List, Optional, Union
@@ -30,23 +31,29 @@ from tqdm import tqdm
 
 # Cell
 def _get_link(x: str):
-    end = x.split('/')[-1]
+    end = x.split("/")[-1]
     return "https://bl.iro.bl.uk/concern/datasets/" + end
 
 # Cell
 @lru_cache(256)
 def get_newspaper_links():
     """Returns titles from the Newspaper Collection"""
-    urls = [f"https://bl.iro.bl.uk/collections/353c908d-b495-4413-b047-87236d2573e3?locale=en&page={page}" for page in range(1, 3)]
+    urls = [
+        f"https://bl.iro.bl.uk/collections/9a6a4cdd-2bfe-47bb-8c14-c0a5d100501f?locale=en&page={page}"
+        for page in range(1, 3)
+    ]
     link_tuples = []
     for url in urls:
         r = requests.get(url)
         r.raise_for_status()
-        soup = BeautifulSoup(r.text, 'lxml')
-        links = soup.find_all("p", class_="media-heading",)
+        soup = BeautifulSoup(r.text, "lxml")
+        links = soup.find_all(
+            "p",
+            class_="media-heading",
+        )
         for link in links:
             link = link.find("a")
-            url = link['href']
+            url = link["href"]
             if url:
                 t = (link.text, _get_link(url))
                 link_tuples.append(t)
@@ -63,9 +70,9 @@ def get_download_urls(url: str) -> list:
         print(E)
 
     soup = BeautifulSoup(r.text, "lxml")
-    link_ends =  soup.find_all('a', id='file_download')
-    urls = ["https://bl.iro.bl.uk" + link['href'] for link in link_ends]
-        #data = json.loads(soup.find("script", type="application/ld+json").string)
+    link_ends = soup.find_all("a", id="file_download")
+    urls = ["https://bl.iro.bl.uk" + link["href"] for link in link_ends]
+    # data = json.loads(soup.find("script", type="application/ld+json").string)
     # except AttributeError as E:
     #     print(E)
     # if data:
@@ -90,7 +97,7 @@ def _download(url: str, dir: Union[str, Path]):
     try:
         r = s.get(url, stream=True, timeout=(30))
         r.raise_for_status()
-        #fname = r.headers["Content-Disposition"].split('_')[1]
+        # fname = r.headers["Content-Disposition"].split('_')[1]
         fname = "_".join(r.headers["Content-Disposition"].split('"')[1].split("_")[0:5])
         if fname:
             with open(f"{dir}/{fname}", "wb") as f:
@@ -112,7 +119,9 @@ def download_from_urls(urls: List[str], save_dir: Union[str, Path], n_threads: i
     logger.add(lambda msg: tqdm.write(msg, end=""))
     with tqdm(total=len(urls)) as progress:
         with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
-            future_to_url = {executor.submit(_download, url, save_dir): url for url in urls}
+            future_to_url = {
+                executor.submit(_download, url, save_dir): url for url in urls
+            }
             for future in future_to_url:
                 future.add_done_callback(lambda p: progress.update(1))
             for future in concurrent.futures.as_completed(future_to_url):
@@ -124,7 +133,7 @@ def download_from_urls(urls: List[str], save_dir: Union[str, Path], n_threads: i
                 else:
                     if data:
                         logger.info(f"{url} downloaded to {data}")
-                        download_count+=1
+                        download_count += 1
         toc = time.perf_counter()
     logger.remove()
     logger.info(f"Downloads completed in {toc - tic:0.4f} seconds")
@@ -135,7 +144,7 @@ def download_from_urls(urls: List[str], save_dir: Union[str, Path], n_threads: i
 def cli(
     save_dir: Param("Output Directory", str),
     n_threads: Param("Number threads to use") = 8,
-    subset: Param("Download subset of HMD", int,opt=True) = None
+    subset: Param("Download subset of HMD", int, opt=True) = None,
 ):
     "Download HMD newspaper from iro to `save_dir` using `n_threads`"
     logger.info("Getting title urls")
@@ -153,14 +162,22 @@ def cli(
     all_urls = list(itertools.chain(*all_urls))
     if subset:
         if len(all_urls) < subset:
-            raise ValueError(f"Size of requested sample {subset} is larger than total number of urls:{all_urls}")
+            raise ValueError(
+                f"Size of requested sample {subset} is larger than total number of urls:{all_urls}"
+            )
         all_urls = random.sample(all_urls, subset)
     print(all_urls)
     download_count = download_from_urls(all_urls, save_dir, n_threads=n_threads)
     request_url_count = len(all_urls)
-    if request_url_count==download_count:
-        logger.info(f"\U0001F600 Requested count of urls: {request_url_count} matches number downloaded: {download_count}")
-    if request_url_count>download_count:
-        logger.warning(f"\U0001F622 Requested count of urls: {request_url_count} higher than number downloaded: {download_count}")
-    if request_url_count<download_count:
-        logger.warning(f"\U0001F937 Requested count of urls: {request_url_count} lower than number downloaded: {download_count}")
+    if request_url_count == download_count:
+        logger.info(
+            f"\U0001F600 Requested count of urls: {request_url_count} matches number downloaded: {download_count}"
+        )
+    if request_url_count > download_count:
+        logger.warning(
+            f"\U0001F622 Requested count of urls: {request_url_count} higher than number downloaded: {download_count}"
+        )
+    if request_url_count < download_count:
+        logger.warning(
+            f"\U0001F937 Requested count of urls: {request_url_count} lower than number downloaded: {download_count}"
+        )
